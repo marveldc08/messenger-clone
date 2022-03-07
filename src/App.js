1deef98e1929945c387752fc5d7e18d0809087d5
David <emekachinye09@gmail.com>
2022-03-07 17:10:11 +0100

    update node_modules folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(['hi', 'yoo', 'whats up']);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    //setInput("");
+  } 
   return (
+
     <div className="App">
       <h1>Hello Clever Programmer! 🚀</h1>
+
+      
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button onClick={sendMessage()}>send message</button>
     </div>
   );
 }
